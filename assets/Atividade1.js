88be1122152ecf5c91f0f55570f4adb90258167c
Luiz F. Picolo <dtommasorodrigues@gmail.com>
2023-07-05 17:06:05 -0400

    Atividade

---
+++ assets/Atividade1.js
@@ -0,0 +1,21 @@
+//Atividade1
+
+let letra = 3;
+
+if ((letra % 4 === 0 && letra % 100 !== 0) || ( letra % 400 === 0) ){
+  console.log('O A é vogal');
+} else { 
+  console.log('O B é consoante');
+}
+
+//Atividade2
+
+let letras = 3;
+
+if ((letras % 4 === 0 && letras % 100 ));
+  console.log('O A é vogal');
+} else { 
+  console.log('O B é consoante');
+}
+
+
