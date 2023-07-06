cfe9c7414ff0efb772deddb7089d1ffd41693734
Luiz F. Picolo <dtommasorodrigues@gmail.com>
2023-07-06 14:03:39 -0400

    Atividade

--- assets/Atividade1.js
+++ assets/Atividade1.js
@@ -10,12 +10,63 @@ if ((letra % 4 === 0 && letra % 100 !== 0) || ( letra % 400 === 0) ){
 
 //Atividade2
 
-let letras = 3;
+let nota = 0;
 
-if ((letras % 4 === 0 && letras % 100 ));
-  console.log('O A é vogal');
-} else { 
-  console.log('O B é consoante');
+if (nota === 100){
+  console.log('A');
+} else if(nota === 80) { 
+  console.log('B');
 }
+ else if (nota === 60) { 
+  console.log('C');
+}
+ else if(nota === 20) { 
+  console.log('D');
+}
+ else if(nota === 0) { 
+  console.log('F');
+}
+
+//Atividade3
+
+let valor = 10
+
+if (valor % 3 === 0 && valor % 5 === 0){
+    console.log('O número ' + valor + ' é divisível por 3 e 5');
+} else { 
+    console.log('O número ' + valor + ' não é divisível por 3 e 5');
+}
+
+//Atividade4
+
+let valor2 = 10
+
+if (valor2 % 2 === 0){
+    console.log('É par');
+} else { 
+    console.log('É impar');
+}
+
+//Atividade5
+
+let valor3 = 10;
+
+if (valor3 > 0){
+    console.log('É positivo');
+} else if( valor3 === 0) { 
+    console.log('É a 0');
+}else{
+  console.log('É negativo');
+}
+
+//Atividade6
+
+
+
+
+
+
+
+
 
 
